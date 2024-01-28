43cee7ef00e9975d0fa4cbb00ae918f2f8c8cdff
EowynStark <andwellm89@gmail.com>
2024-01-27 18:32:01 -0600

    adding .get route to findAll

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,17 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// finds all categories and includes associated Products
+router.get('/', async (req, res) => {
+ try {
+  const categories = await Category.findAll({
+    include: Product,
+  });
+  res.json(categories);
+ }catch (error) {
+  console.error(error);
+  res.status(500).json({error: 'Internal Server Error'});
+ }
 });
 
 router.get('/:id', (req, res) => {
